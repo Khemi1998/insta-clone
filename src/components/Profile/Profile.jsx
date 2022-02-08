515903aac48e8a50baf17efccec73cb5b7c17f62
Khemi <khemi@Khemis-Laptop.local>
2022-02-08 20:00:54 +0000

    added styling to bio

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,6 @@
 import "./Profile.scss";
+import verified from "../../assets/images/verified.svg";
+import dots from "../../assets/images/dots.svg";
 
 const Profile = (props) => {
   const { dp, Name, bioTitle, description, posts, followers, following } =
@@ -7,25 +9,29 @@ const Profile = (props) => {
   return (
     <div className="profile">
       <div className="profile__head__container">
-        <img src={dp} alt="profile" />
+        <img className="profile__head__pic" src={dp} alt="profile" />
         <div className="profile__connect">
           <div className="profile__head">
             <h2>{Name}</h2>
+            <img src={verified} alt="verified" />
+            <img src={dots} alt="dots" />
           </div>
+          <button className="profile__followbtn">Follow</button>
+          <button className="profile__arrowbtn">˅</button>
         </div>
       </div>
-      <strong>{bioTitle}</strong>
-      <p>{description}</p>
+      <strong className="profile__content">{bioTitle}</strong>
+      <p className="profile__content">{description}</p>
       <div className="profile__info">
-        <div className="profile__info__posts">
+        <div className="profile__info__item">
           <strong>{posts}</strong>
           <p>posts</p>
         </div>
-        <div className="profile__info__followers">
+        <div className="profile__info__item">
           <strong>{followers}</strong>
           <p>followers</p>
         </div>
-        <div className="profile__info__following">
+        <div className="profile__info__item">
           <strong>{following}</strong>
           <p>following</p>
         </div>
