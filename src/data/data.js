af9223f3d92d76b86d0590f1954341fb6025842e
Khemi <khemi@Khemis-Laptop.local>
2022-02-08 21:16:55 +0000

    adding margin

--- src/data/data.js
+++ src/data/data.js
@@ -1,33 +1,42 @@
 const profiles = [
-    {
-        id: 1,
-        name: "Cat",
-        dp: "http://placekitten.com/100/100",
-        bio: "My cat's account",
-        description: "A document of my cat's adventures",
-        posts:"16,889",
-        followers:"13.1m",
-        following: "7"
-    },
-    {
-        id: 2,
-        name: "Dog",
-        dp: "https://placedog.net/100/100",
-        bio: "My dog's account",
-        description: "A document of my dog's adventures",
-        posts:"12,5569",
-        followers:"9.1m",
-        following: "1"
-    },
-    {
-        id: 3,
-        name: "Rabbit",
-        dp: "https://media.istockphoto.com/photos/fluffy-funny-ginger-rabbit-on-a-background-of-green-nature-picture-id1307795728?k=20&m=1307795728&s=612x612&w=0&h=MeKODHarCXtl1jL3zCNpJnlAhapCfT5cxNr6n89hK-I=",
-        bio: "My Rabbit's account",
-        description: "A document of my Rabbit's adventures",
-        posts:"25,999",
-        followers:"15.6m",
-        following: "108"
-    }
-]
+  {
+    id: 1,
+    name: "Cat",
+    dp: "http://placekitten.com/100/100",
+    bio: "My cat's account",
+    description: "A document of my cat's adventures",
+    posts: "16,889",
+    followers: "13.1m",
+    following: "7",
+    postings: [
+      "http://placekitten.com/200/200",
+      "http://placekitten.com/300/300",
+      "http://placekitten.com/400/400",
+      "http://placekitten.com/500/500",
+      "http://placekitten.com/600/600",
+      "http://placekitten.com/700/700",
+      "http://placekitten.com/800/800",
+    ],
+  },
+  {
+    id: 2,
+    name: "Dog",
+    dp: "https://placedog.net/100/100",
+    bio: "My dog's account",
+    description: "A document of my dog's adventures",
+    posts: "12,5569",
+    followers: "9.1m",
+    following: "1",
+  },
+  {
+    id: 3,
+    name: "Rabbit",
+    dp: "https://media.istockphoto.com/photos/fluffy-funny-ginger-rabbit-on-a-background-of-green-nature-picture-id1307795728?k=20&m=1307795728&s=612x612&w=0&h=MeKODHarCXtl1jL3zCNpJnlAhapCfT5cxNr6n89hK-I=",
+    bio: "My Rabbit's account",
+    description: "A document of my Rabbit's adventures",
+    posts: "25,999",
+    followers: "15.6m",
+    following: "108",
+  },
+];
 export default profiles;
