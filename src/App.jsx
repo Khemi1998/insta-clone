41b8175a85fcb18d3d06ba0cd8defc5c632ddd7b
Khemi <khemi@Khemis-Laptop.local>
2022-02-08 16:13:07 +0000

    setup complete

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.scss';
+
+function App() {
+  return (
+    <Router>
+      <Nav />
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/greetings" element={<GreetingList greetings={greetings} />} />
+        <Route path="/new-greeting" element={<Form />} /> */}
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
